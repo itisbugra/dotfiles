462aead49453c53e95e39cb1db14e58f8cbfac1b
Buğra Ekuklu <ekuklu@icloud.com>
2023-08-15 16:39:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# oh-my-zsh & plugins
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+
+# zsh
+cp ./.zshrc ~/
+
+# antigen (zsh)
 mkdir ~/.antigen
 curl -L git.io/antigen > ~/.antigen/antigen.zsh
